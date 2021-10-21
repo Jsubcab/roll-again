13a627c1c8d2a15681025723e6892e4a0956ad74
JSC <jsubcab@gmail.com>
2021-10-22 00:15:09 +0200

    [TFG-1] - Base

---
+++ src/main/java/rollagain/users/UsersImpl.java
@@ -0,0 +1,17 @@
+package rollagain.users;
+
+class UsersImpl implements Users {
+
+    private int id;
+    private String username;
+    private String password;
+    private String city;
+    private int zipcode;
+    private int phone;
+    private String email;
+
+    @Override
+    public void pepe() {
+
+    }
+}
